9d903d2af92234e3b8e9800f01ddc278fa530092
Shane McDonough <shanemcdman@gmail.com>
2020-12-18 09:03:38 -0800

    Create Enums

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+enum Piece {
+    Empty,
+    Black(Type),
+    White(Type),
+}
+enum Type {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King,
+}
 fn main() {
     println!("Hello, world!");
 }
